9745a78bcda2d3e45ed845406b2211ebe5f026ed
hussain <h2002ahmed@gmail.com>
2025-05-15 17:05:25 +0500

    added .env file access ips from .env

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
+import sys
 from collections import Counter
+
+import nltk
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from flask import Flask, jsonify, render_template, request
 from PyPDF2 import PdfReader
 from werkzeug.utils import secure_filename
 
-from client import load_text, process_chunk, split_text  # your backend logic
+from client import load_text, preprocess_text, process_chunk, split_text
+
+nltk.download("stopwords")
+nltk.download("punkt_tab")
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
@@ -14,11 +23,28 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 def index():
-    return render_template("index.html")  # this serves index.html from templates/
+    return render_template("index.html")
 
 
 @app.route("/process", methods=["POST"])
 def process_file():
+
+    # # Configuration
+    # # slave_servers = [
+    # #     ("172.31.35.88", 18861),
+    # #     ("172.31.35.148", 18862),
+    # #     ("172.31.39.162", 18863),
+    # # ]
+
+    slave_servers = [
+        (os.getenv("SLAVE1_IP"), int(os.getenv("SLAVE1_PORT"))),
+        (os.getenv("SLAVE2_IP"), int(os.getenv("SLAVE2_PORT"))),
+        (os.getenv("SLAVE3_IP"), int(os.getenv("SLAVE3_PORT"))),
+    ]
+
+    print(slave_servers)
+
+    ports = [18861, 18862, 18863]
     text = ""
 
     # Check if file was uploaded
@@ -51,15 +77,46 @@ def process_file():
     else:
         return jsonify({"error": "No valid input provided."}), 400
 
-    ports = [18861, 18862, 18863]
+    text = preprocess_text(text)
+    # Split text into chunks
     chunks = split_text(text, len(ports))
-    results = [process_chunk(port, chunk) for port, chunk in zip(ports, chunks)]
+    print(f"Split text into {len(chunks)} chunks")
 
+    # # Connect and send tasks
+    # results = [process_chunk(port, chunk) for port, chunk in zip(ports, chunks)]
+
+    # Connect and send tasks
+    results: list[dict] = []
+    for port, chunk in zip(ports, chunks):
+        try:
+            word_counts = process_chunk(port, chunk)
+            if word_counts:
+                results.append(word_counts)
+                print(f"Successfully processed chunk on port {port}")
+            else:
+                print(f"No results from slave on port {port}")
+        except Exception as e:
+            print(f"Failed to process on port {port}: {e}")
+
+    # Combine results
+    if not results:
+        print("Error: No results collected from any slaves.")
+        sys.exit(1)
+
+    # aggregate result
     final_counts = Counter()
     for r in results:
         final_counts.update(r)
 
-    # Send all words, not just top 10
+    print("\nTop 10 most common words:")
+    print("-" * 30)
+    # for word, count in results.most_common(10):
+    #     print(f"{word}: {count}")
+
+    for word, count in final_counts.most_common(10):
+        print(f"{word}: {count}")
+
+    # Send all words
     words = [[word, count] for word, count in final_counts.items()]
     return jsonify({"words": words})
 
